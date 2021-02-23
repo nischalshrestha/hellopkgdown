b492b2c8a763a0d59a581a9e2c5ce9fee347050f
nischalshrestha <bitsorific@gmail.com>
2021-02-23 12:32:44 -0500

    first commit

---
+++ R/helloworld.R
@@ -0,0 +1,11 @@
+
+#' Prints Hello, World! to the console.
+#'
+#' @return No value returned
+#' @export
+#'
+#' @examples
+#' helloworld()
+helloworld <- function() {
+  message("Hello, World!")
+}
